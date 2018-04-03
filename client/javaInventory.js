7705c8c8adc9464b3a61b235146cd52a09b81d1f
SiebeGerritsen <33697618+SiebeGerritsen@users.noreply.github.com>
2018-04-03 15:23:58 +0200

    Inventory systeem werkend

---
+++ client/javaInventory.js
@@ -0,0 +1,67 @@
+﻿Inventory = function () {
+    var self = {
+        items : []
+    }
+
+    self.addItem = function (id, amount) {
+        for (var i = 0; i < self.items.length; i++) {
+            if (self.items[i].id === id) {
+                self.items[i].amount += amount;
+                self.refreshRender();
+                return;
+            }
+        }
+        self.items.push({ id: id, amount: amount })
+        self.refreshRender();
+    }
+
+    self.removeItem = function (id, amount) {
+        for (var i = 0; i < self.items.length; i++) {
+            if (self.items[i].id === id) {
+                self.items[i].amount -= amount;
+                if (self.items[i].amount <= 0)
+                    self.items.splice(1, 1);
+                self.refreshRender();
+                return;
+            }
+        }
+    }
+
+    self.hasItem = function (id, amount) {
+        for (var i = 0; i < self.items.length; i++) {
+            if (self.items[i].id === id) {
+                return self.items[i].amount >= amount;
+            }
+        }
+        return false;
+    }
+
+    self.refreshRender = function () {
+        var str = " ";
+        for (var i = 0; i < self.items.length; i++) {
+            let item = Item.List[self.items[i].id];
+            str += item.name + " ";
+        }
+
+        document.getElementById("inventory").innerHTML = str;
+    }
+
+    return self;
+}
+
+Item = function (id, name, event) {
+    var self = {
+        id: id,
+        name: name,
+        event: event,
+    }
+    Item.List[self.id] = self;
+    return self;
+}
+Item.List = {};
+
+Item("test", "Test", function () {
+    textContent = "test removed";
+    playerInventory.removeItem("test", 1);
+});
+
